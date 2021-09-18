567e07e326ce3fdd9a372f8c71cf8c8f8aeeaaa2
Sr. Meia-noite { λ } (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2021-09-18 07:50:32 -0300

    ;)

--- testing.py
+++ testing.py
@@ -3,15 +3,16 @@ def paint(string, rgb, foreground = True, end = True):
     r, g, b = rgb
     return '\x1b' + f'[{38 if foreground else 48};2;{r};{g};{b}m{string}' + ("\x1b[00m" if end else "")
   
-def main():
-    from time    import sleep      as wait
+def rprint(string):
     from rainbow import rainbowRGB as rRGB
-
-    for n in range(1, 100):
+    
+    # Loops throught each letter and applies an color.
+    for n in range(len(string)):
         print(
-            paint("| ##### @@@ $ @@@ ##### |", rRGB(n))
+            paint(string[n], rRGB(n + 1)), end = ""
         )
 
-        wait(0.02)
+def main():
+    rprint("#### Yeah, this seems nice. #####")
 
 if __name__ == '__main__': main()
