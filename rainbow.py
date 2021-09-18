cfc800dd583df5923922c85e0ded9ee21e1688f0
Sr. Meia-noite { λ } (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2021-09-18 07:36:20 -0300

    Phases.

--- rainbow.py
+++ rainbow.py
@@ -1,8 +1,8 @@
-def rainbowRGB(n, frequency = .1, waveCenter = 128, waveWidth = 127):
+def rainbowRGB(n, frequency = .1, waveCenter = 128, waveWidth = 127, rPhase = 2, gPhase = 0, bPhase = 4):
     from math import sin
 
-    r = int(sin(frequency * n + 2) * waveWidth + waveCenter)
-    g = int(sin(frequency * n + 0) * waveWidth + waveCenter)
-    b = int(sin(frequency * n + 4) * waveWidth + waveCenter)
+    r = int(sin(frequency * n + rPhase) * waveWidth + waveCenter)
+    g = int(sin(frequency * n + gPhase) * waveWidth + waveCenter)
+    b = int(sin(frequency * n + bPhase) * waveWidth + waveCenter)
     
     return (r, g, b)
