f7500ffd1a58e8477b9b8e2ad71189c72621aa76
Sr. Meia-noite { λ } (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2021-09-18 07:29:11 -0300

    There it is.

---
+++ rainbow.py
@@ -0,0 +1,8 @@
+def rainbowF(n, frequency = .1, waveCenter = 128, waveWidth = 127):
+    from math import sin
+
+    r = int(sin(frequency * n + 2) * waveWidth + waveCenter)
+    g = int(sin(frequency * n + 0) * waveWidth + waveCenter)
+    b = int(sin(frequency * n + 4) * waveWidth + waveCenter)
+    
+    return (r, g, b)
